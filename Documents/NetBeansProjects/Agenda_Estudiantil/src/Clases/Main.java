f1b1e4d3d9c4de28b24ce69239016f77d13383a1
JeanM1996 <jeanpaulmosquera1996@hotmail.com>
2016-07-16 17:51:08 -0500

    Final: Añadido Documentacion y otros

--- Documents/NetBeansProjects/Agenda_Estudiantil/src/Clases/Main.java
+++ Documents/NetBeansProjects/Agenda_Estudiantil/src/Clases/Main.java
@@ -1,20 +1,13 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Clases;
 
 import interfaces.Agenda;
 
 /**
- *
- * @author LAB4-08
+ * @author Jean Mosquera
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Agenda iniciar=new Agenda();
         iniciar.setVisible(true);
